b1fa4d883841f07b763ebc7ee5205b54d9c6d74c
Zkrallah <muhammad.heshamyt@gmail.com>
2022-08-24 17:05:49 +0200

    min, max, humidity and ui implemented

--- app/src/main/java/com/dimits/simpleweatherapp/WeatherActivity.kt
+++ app/src/main/java/com/dimits/simpleweatherapp/WeatherActivity.kt
@@ -1,17 +1,16 @@
 package com.dimits.simpleweatherapp
 
+import android.annotation.SuppressLint
 import android.location.Address
 import android.location.Geocoder
-import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.dimits.simpleweatherapp.databinding.ActivityMainBinding
 import com.dimits.simpleweatherapp.databinding.ActivityWeatherBinding
-import com.dimits.simpleweatherapp.model.Weather
+import com.dimits.simpleweatherapp.model.Response
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
-import retrofit2.Retrofit
+import java.text.SimpleDateFormat
 import java.util.*
 
 class WeatherActivity : AppCompatActivity() {
@@ -42,15 +41,35 @@ class WeatherActivity : AppCompatActivity() {
 
     private fun getWeather() {
 
-        API.apiService.getMainResponse(city,key).subscribeOn(Schedulers.io())
+        API.apiService.getMainResponse(city,"metric",key).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread()).subscribe(
             {
-                binding.mainTemp.text = "current temperature : ${it.main?.temp.toString()}"
+                fillTheData(it)
             },
             {
                 Log.d(TAG, "getWeather: $it")
             }
             )
+    }
+
+    private fun fillTheData(response: Response) {
+        binding.timeUpdated.text = "Updated At: ${getDateTime(response.dt.toString())}"
+        binding.mainTemp.text = response.main?.temp.toString()
+        binding.maxTemp.text = response.main?.tempMax.toString()
+        binding.minTemp.text = response.main?.tempMin.toString()
+        binding.humidity.text = response.main?.humidity.toString() + " %"
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getDateTime(s: String): String? {
+        try {
+            val sdf = SimpleDateFormat("MM/dd/yyyy hh:mm a")
+            val netDate = Date(s.toLong() * 1000)
+            return sdf.format(netDate)
+        } catch (e: Exception) {
+            return e.toString()
+        }
     }
 
     companion object{
